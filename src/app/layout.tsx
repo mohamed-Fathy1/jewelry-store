873f99ce9c309680002f44a207d30ea4dc3b6e93
mohamedFathygeeklabs <mohamedfathygeeklabs@gmail.com>
2025-04-18 13:52:51 +0200

    Refactor Facebook Pixel tracking code in RootLayout for improved analytics setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,8 +38,34 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Meta Pixel Code */}
-        <Script id="facebook-pixel" strategy="afterInteractive">
+        {/* Meta Pixel Code - First Pixel */}
+        <Script id="facebook-pixel-1" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1389492105728089');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1389492105728089&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End First Meta Pixel Code */}
+
+        {/* Meta Pixel Code - Second Pixel */}
+        <Script id="facebook-pixel-2" strategy="afterInteractive">
           {`
             !function(f,b,e,v,n,t,s)
             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -62,8 +88,7 @@ export default function RootLayout({
             alt=""
           />
         </noscript>
-
-        {/* End Meta Pixel Code */}
+        {/* End Second Meta Pixel Code */}
       </head>
       <body
         className={inter.className}
